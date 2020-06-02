79c170afd546a6495fcafad4d4e03107b0b18d1f
neel <nsneeladrisen@gmail.com>
2020-06-02 19:27:17 +0530

    first commit

---
+++ insertions_myProject.sql
@@ -0,0 +1,363 @@
+-- entries into insertion table
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek1 Sen","abhishek.sen@accoliteindia.com","abhishek1.sen@gmail.com","mumbai");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek2 Sen","abhishek2.sen@accoliteindia.com","abhishek2.sen@gmail.com","mumbai");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek3 Sen","abhishek3.sen@accoliteindia.com","abhishek3.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek4 Sen","abhishek4.sen@accoliteindia.com","abhishek4.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek5 Sen","abhishek5.sen@accoliteindia.com","abhishek5.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek6 Sen","abhishek6.sen@accoliteindia.com","abhishek6.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek7 Sen","abhishek7.sen@accoliteindia.com","abhishek7.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek8 Sen","abhishek8.sen@accoliteindia.com","abhishek8.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek9 Sen","abhishek9.sen@accoliteindia.com","abhishek9.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek10 Sen","abhishek10.sen@accoliteindia.com","abhishek10.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek11 Sen","abhishek11.sen@accoliteindia.com","abhishek11.sen@gmail.com","gurgaon");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek12 Sen","abhishek12.sen@accoliteindia.com","abhishek12.sen@gmail.com","gurgaon");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek13 Sen","abhishek13.sen@accoliteindia.com","abhishek13.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek14 Sen","abhishek14.sen@accoliteindia.com","abhishek14.sen@gmail.com","bangalore");
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek15 Sen","abhishek15.sen@accoliteindia.com","abhishek15.sen@gmail.com","bangalore");
+
+
+insert into employee (name,company_email,personal_email,location)
+values ("Abhishek16 Sen","abhishek16.sen@accoliteindia.com","abhishek16.sen@gmail.com","bangalore");
+
+-- select * from employee;
+
+-- ---------------------------------------------------------------------
+-- insert phone_directory
+-- ---------------------------------------------------------------------
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000002,2);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000003,3);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000004,4);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000005,5);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000006,6);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000007,7);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000008,8);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000009,9);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000010,10);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000011,11);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000012,12);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000013,13);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000014,14);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000015,15);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9000000016,16);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9100000001,1);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9200000002,2);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9300000003,3);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9400000004,4);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9500000005,5);
+
+insert into phone_directory (country_code,ph_no,emp_id)
+values(091,9600000006,6);
+
+select* from phone_directory;
+
+-- --------------------------------------------------------
+-- education
+-- --------------------------------------------------------
+select*from education;
+insert into education (emp_id,college,university,degree)
+values (1,"college1","university1","degree1");
+
+insert into education (emp_id,college,university,degree)
+values (2,"college2","university2","degree2");
+
+insert into education (emp_id,college,university,degree)
+values (3,"college3","university3","degree3");
+
+insert into education (emp_id,college,university,degree)
+values (4,"college4","university4","degree4");
+
+insert into education (emp_id,college,university,degree)
+values (5,"college5","university5","degree5");
+
+insert into education (emp_id,college,university,degree)
+values (6,"college6","university6","degree6");
+
+insert into education (emp_id,college,university,degree)
+values (7,"college7","university7","degree7");
+
+insert into education (emp_id,college,university,degree)
+values (8,"college8","university8","degree8");
+
+insert into education (emp_id,college,university,degree)
+values (9,"college9","university9","degree9");
+
+insert into education (emp_id,college,university,degree)
+values (10,"college10","university10","degree10");
+
+insert into education (emp_id,college,university,degree)
+values (11,"college11","university11","degree11");
+
+insert into education (emp_id,college,university,degree)
+values (12,"college12","university12","degree12");
+
+insert into education (emp_id,college,university,degree)
+values (13,"college3","university3","degree3");
+
+insert into education (emp_id,college,university,degree)
+values (14,"college14","university14","degree14");
+
+insert into education (emp_id,college,university,degree)
+values (15,"college15","university15","degree15");
+
+insert into education (emp_id,college,university,degree)
+values (16,"college16","university16","degree16");
+
+insert into education (emp_id,college,university,degree)
+values (16,"college16-1","university16-1","degree16-1");
+
+insert into education (emp_id,college,university,degree)
+values (1,"college1-1","university1-1","degree1-1");
+
+insert into education (emp_id,college,university,degree)
+values (2,"college2-1","university2-1","degree2-1");
+
+-- ------------------------------------------------------------------
+-- document
+-- ------------------------------------------------------------------
+insert into document (emp_id,type,document)
+values(1,"resume",LOAD_FILE('C:\Users\nsnee\abhishek\database\documents\doc1.pdf'));
+
+insert into document (emp_id,type,document)
+values(2,"resume",LOAD_FILE('C:\Users\nsnee\abhishek\database\documents\doc1.pdf'));
+
+-- --------------------------------------------------------------------------------------
+-- skill insertions
+-- --------------------------------------------------------------------------------------
+insert into skill (skill_name)
+values ("java");
+
+insert into skill (skill_name)
+values ("spring boot");
+
+insert into skill (skill_name)
+values ("angular");
+
+insert into skill (skill_name)
+values ("javascript");
+
+select* from skill;
+-- -------------------------------------------------
+-- employee_skillset tabel
+-- -------------------------------------------------
+insert into employee_skillset (emp_id , skill_id)
+values (1,1);
+
+insert into employee_skillset (emp_id , skill_id)
+values (2,1);
+
+insert into employee_skillset (emp_id , skill_id)
+values (3,1);
+
+insert into employee_skillset (emp_id , skill_id)
+values (4,1);
+
+insert into employee_skillset (emp_id , skill_id)
+values (1,2);
+
+insert into employee_skillset (emp_id , skill_id)
+values (2,2);
+
+insert into employee_skillset (emp_id , skill_id)
+values (3,2);
+
+insert into employee_skillset (emp_id , skill_id)
+values (4,2);
+
+insert into employee_skillset (emp_id , skill_id)
+values (1,3);
+
+insert into employee_skillset (emp_id , skill_id)
+values (2,3);
+
+insert into employee_skillset (emp_id , skill_id)
+values (3,3);
+
+insert into employee_skillset (emp_id , skill_id)
+values (4,3);
+
+insert into employee_skillset (emp_id , skill_id)
+values (1,4);
+
+insert into employee_skillset (emp_id , skill_id)
+values (2,4);
+
+insert into employee_skillset (emp_id , skill_id)
+values (3,4);
+
+
+select * from employee_skillset;
+
+-- ----------------------------------------------
+-- project
+-- --------------------------------------------
+
+insert into project (manager_id,project_name)
+values(10,"project1");
+
+insert into project (manager_id,project_name)
+values(11,"project2");
+
+insert into project (manager_id,project_name)
+values(12,"project3");
+
+insert into project (manager_id,project_name)
+values(13,"project4");
+
+
+
+select* from project;
+
+-- -----------------------------------------------
+-- proect alloation
+-- -----------------------------------------------
+
+insert into project_allocation (emp_id,proj_id,start_date,end_date)
+values(1,1,'2020-01-01','2020-05-30');
+
+insert into project_allocation (emp_id,proj_id,start_date,end_date)
+values(2,1,'2020-01-01','2020-04-30');
+
+insert into project_allocation (emp_id,proj_id,start_date,end_date)
+values(3,2,'2020-01-01','2020-02-25');
+
+insert into project_allocation (emp_id,proj_id,start_date,end_date)
+values(4,3,'2020-01-01','2020-03-30');
+
+select* from project_allocation;
+
+
+-- ---------------------------------------------------------
+-- hiering manager
+-- ---------------------------------------------------------
+insert into hiring_manager (name,email,ph_no,country_code,team_name,dept)
+values("manager1","email1@gmail.com",911111111,091,"team1","dept1");
+
+insert into hiring_manager (name,email,ph_no,country_code,team_name,dept)
+values("manager2","email2@gmail.com",911111112,091,"team2","dept2");
+
+-- --------------------------------------------------------------
+-- demand
+-- --------------------------------------------------------------
+
+-- select* from hiring_manager;
+insert into demand (location , experience_requirement , number_people,creation_date,h_id) 
+values("mumbai" , 2, 5,'2020-05-1','1'); 
+
+insert into demand (location , experience_requirement , number_people,creation_date,h_id) 
+values("mumbai" , 3, 6,'2020-04-1',1); 
+
+insert into demand (location , experience_requirement , number_people,creation_date,h_id) 
+values("mumbai" , 2, 5,'2020-05-1',2); 
+
+insert into demand (location , experience_requirement , number_people,creation_date,h_id) 
+values("mumbai" , 2, 5,'2020-05-1',2); 
+
+
+-- select * from demand;
+
+-- ----------------------------------------------------------------------------------
+-- demand skillset
+select * from skill;
+-- ----------------------------------------------------------------------------------
+insert into demand_skillset (dem_id, skill_id)
+values (1,1);
+
+insert into demand_skillset (dem_id, skill_id)
+values (1,2);
+
+insert into demand_skillset (dem_id, skill_id)
+values (1,3);
+
+insert into demand_skillset (dem_id, skill_id)
+values (2,1);
+
+insert into demand_skillset (dem_id, skill_id)
+values (2,4);
+
+
+commit
+
+ 
+
+
+
+
+-- ALTER TABLE demand MODIFY dem_id INT UNSIGNED NOT NULL AUTO_INCREMENT;
+
+
+
+
+
+
+
+
---
+++ select queries.sql
@@ -0,0 +1,82 @@
+-- select employee by project alocation
+-- --------------------------------------------------
+select emp_id,proj_id from project_allocation 
+where DATE(end_date) between '2020-04-25' and '2020-05-31' ;
+
+select * from employee where emp_id = 2;
+
+select* from employee where company_email = "abhishek.sen@accoliteindia.com";
+
+select* from employee where personal_email = "abhishek1@gmail.com";
+
+select * from employee where location = "mumbai";
+
+select * from skill where skill_id = 1;
+
+-- all skill of employee with id =1
+
+select skill.skill_id, skill.skill_name 
+from skill
+join employee_skillset  on  employee_skillset.skill_id =  skill.skill_id
+where employee_skillset.emp_id = 1 ;
+-- ----------------------------------
+
+-- all employee with skill id = 1
+
+select employee.emp_id , employee.name , employee.company_email , employee.personal_email , employee.location
+from employee
+join employee_skillset on employee_skillset.emp_id = employee.emp_id
+where employee_skillset.skill_id =1;
+
+
+-- --------------
+-- demand
+
+select * from demand ;
+
+select * from demand where creation_date <= "2020-04-05" ;
+ 
+ 
+ select * from demand where location = "Mumbai";
+ 
+ 
+ select * from demand where number_people >5;
+ 
+ -- ------------------
+ -- get all demand with skill id
+ 
+ select demand.dem_id,demand.creation_date,demand.experience_requirement,demand.h_id,demand.location,demand.number_people
+ from demand
+ join demand_skillset on demand.dem_id = demand_skillset.dem_id
+ where demand_skillset.skill_id = 1;
+ 
+ select* from demand_skillset;
+ 
+ 
+ -- get all skills for demand with id
+ select skill.skill_id, skill.skill_name
+ from skill
+ join demand_skillset on demand_skillset.skill_id = skill.skill_id
+ where demand_skillset.dem_id = 1;
+ 
+ select* from employee where lower(name) = "abhishek1 sen";
+ 
+ 
+select* from employee;
+
+select* from hiring_manager;
+
+select* from hiring_manager;
+
+select* from hiring_manager where h_id = 1;
+
+select * from hiring_manager where lower(name) = "manager1";
+
+select * from hiring_manager where lower(email) = "email1@gmail.com";
+
+
+
+
+
+
+
